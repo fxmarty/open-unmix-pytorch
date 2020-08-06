fixed some errors, changed normalization and slicing order in open-unmix model to make it consistent with normalization implementation in deep-u-net

model.py
@@ -139,7 +139,7 @@ class OpenUnmix(nn.Module):
         self.normalize_input = normalization.Normalize(normalization_style,
                                                        input_mean,
                                                        input_scale,
-                                                       self.nb_bins)
+                                                       self.nb_output_bins)
         
         self.fc1 = Linear(
             self.nb_bins*nb_channels, hidden_size,
@@ -194,13 +194,13 @@ class OpenUnmix(nn.Module):
 
         mix = x.detach().clone()
         #print(mix.shape)
+        
+        # shift and scale input to mean=0 std=1 (across all frames in one freq bin)
+        x = self.normalize_input(x)
 
         # crop, because we don't necessarily keep all bins due to the bandwidth
         x = x[..., :self.nb_bins]
         #print(x.shape)
-        
-        # shift and scale input to mean=0 std=1 (across all frames in one freq bin)
-        x = self.normalize_input(x)
 
         # to (nb_frames*nb_samples, nb_channels*nb_bins)
         # and encode to (nb_frames*nb_samples, hidden_size)

normalization.py
@@ -7,7 +7,7 @@ class Normalize(nn.Module):
                 normalization_style,
                 input_mean=None,
                 input_scale=None,
-                nb_bins=None
+                nb_total_bins=None
     ):
         super(Normalize,self).__init__()
         
@@ -15,17 +15,17 @@ class Normalize(nn.Module):
         
         if input_mean is not None:
             input_mean = torch.from_numpy(
-                -input_mean[:nb_bins]
+                -input_mean
             ).float()
         else:
-            input_mean = torch.zeros(nb_bins)
+            input_mean = torch.zeros(nb_total_bins)
         
         if input_scale is not None:
             input_scale = torch.from_numpy(
-                1.0/input_scale[:nb_bins]
+                1.0/input_scale
             ).float()
         else:
-            input_scale = torch.ones(nb_bins)
+            input_scale = torch.ones(nb_total_bins)
 
         self.input_mean = nn.Parameter(input_mean)
         self.input_scale = nn.Parameter(input_scale)

train.py
@@ -99,6 +99,9 @@ def get_statistics(args, dataset):
 
 
 def main():
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    
     parser = argparse.ArgumentParser(description='Open Unmix Trainer')
 
     # which target do we want to train?
@@ -189,12 +192,12 @@ def main():
     if args.normalization_style == None and args.modelname == "open-unmix":
         args.normalization_style = "overall"
         print("Normalization style set by default to \""
-                + args.normalization_style + "\"."
+                + args.normalization_style + "\".")
     
     if args.normalization_style == None and args.modelname == "deep-u-net":
         args.normalization_style = "batch-specific"
         print("Normalization style set by default to \""
-                + args.normalization_style + "\"."
+                + args.normalization_style + "\".")
     
     use_cuda = not args.no_cuda and torch.cuda.is_available()
     print("Using GPU:", use_cuda)
@@ -264,13 +267,13 @@ def main():
         train_dataset.sample_rate, args.nfft, args.bandwidth
     ) # to stay under 16 000 Hz
 
+    if args.model or args.normalization_style == "batch-specific":
+        scaler_mean = None
+        scaler_std = None
+    else:
+        scaler_mean, scaler_std = get_statistics(args, train_dataset)
+
     if args.modelname == "open-unmix":
-        if args.model:
-            scaler_mean = None
-            scaler_std = None
-        else:
-            scaler_mean, scaler_std = get_statistics(args, train_dataset)
-        
         unmix = model.OpenUnmix(
             normalization_style=args.normalization_style,
             input_mean=scaler_mean,
@@ -288,7 +291,11 @@ def main():
             normalization_style=args.normalization_style,
             n_fft=args.nfft,
             n_hop=args.nhop,
-            nb_channels=args.nb_channels
+            nb_channels=args.nb_channels,
+            input_mean=scaler_mean,
+            input_scale=scaler_std,
+            max_bin=max_bin,
+            sample_rate=train_dataset.sample_rate
         ).to(device)
         
     optimizer = torch.optim.Adam(